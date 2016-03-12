2b2e51c404349d24b57832264c917476976250d0
Joe <joe@ahri.tw>
2016-03-12 16:10:20 +0800

    Update BR to 2.27

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -40,7 +40,7 @@ Route::post('game/end/{platform}/{gameId}', 'EndGameController@NewEndedGame');
 
 Route::get('version', function()
 {
-    return 225;	
+    return 227;	
 });
 
 Route::get('/', function()
