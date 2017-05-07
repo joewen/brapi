b09b06721bad2fe33a1bdfc9c53ba72378d71361
Joe <joe@ahri.tw>
2017-05-07 21:29:39 +0800

    BR to 2.37

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -40,7 +40,7 @@ Route::post('game/end/{platform}/{gameId}', 'EndGameController@NewEndedGame');
 
 Route::get('version', function()
 {
-    return 236;	
+    return 237;	
 });
 
 Route::get('/', function()
